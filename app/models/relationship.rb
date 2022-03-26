90d6ef59a6d3820a5f7837a738b26b613f39594e
EC2 Default User <ec2-user@ip-172-31-93-125.ec2.internal>
2022-03-26 02:15:54 +0000

    [update]assotiation trouble resolve

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,6 @@
 class Relationship < ApplicationRecord
-  belongs_to :followed, class_name: "User"
-  belongs_to :follower, class_name: "Follower"
-
+  belongs_to :user, foreign_key: "followed_id"
+  belongs_to :follower, foreign_key: "follower_id"
+  # class_name: "User",
+  # class_name: "Follower",
 end
