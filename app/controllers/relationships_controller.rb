d17b9daa33a978952f982434c3be4cdff67fb263
EC2 Default User <ec2-user@ip-172-31-93-125.ec2.internal>
2022-03-26 12:48:59 +0000

    [update]create destroy function available

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,33 @@
 class RelationshipsController < ApplicationController
-  
+
   def create
-    @user = 
+    # binding.pry
+    user = User.find(params[:follower_id]) #current_user
+    followed_user = User.find(params[:user_id])
+    #followersテーブルにcurrent_userのfollowerとしてのデータがなければ
+    #followersテーブルに新規登録する
+    unless Follower.exists?(user_id: user.id)
+      follower = Follower.new(user_id: user.id, user_name: user.name)
+      follower.save
+    end
+
+    #followed_userのfollowerグループにfollower_userを追加する
+    follower_user = Follower.find_by(user_id: user.id)
+    if followed_user.followers << follower_user
+    #request.refererで遷移前のURLを保持している
+      redirect_to request.referer
+    else
+      @users = User.all
+      @book = Book.new
+      render 'users/index'
+    end
   end
-  
+
   def destroy
-    
+    # binding.pry
+    relationship = Relationship.find(params[:id])
+    relationship.destroy
+    redirect_to request.referer
   end
-  
+
 end
